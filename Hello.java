fee81409421a74d81f7a88e9ec70a95ef6f08fbf
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2025-06-26 18:33:16 +0530

    Primitive datatypes

--- Hello.java
+++ Hello.java
@@ -2,5 +2,17 @@ class Hello
 {
     public static void main (String a[]){
         System.out.print("Hello");
+
+        int number = 125;
+        //float decimal = 5.8;  //will give error, as double is default in java and it's aiming for a double datatype
+        float decimal = 5.8f; //won't give error
+
+        double double_Decimal = 5.8; //won't give error
+
+        char character = 'a';
+
+        boolean boolean_word = true;
+
+
     }
 }
