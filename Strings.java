9406d954e89479d92c24dd7294410620e6e76600
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2025-07-02 21:51:08 +0530

    Concept of stack and heap in strings

--- Strings.java
+++ Strings.java
@@ -14,5 +14,24 @@ public class Strings {
         System.out.println(first_name.hashCode());
         System.out.println(first_name.charAt(3));
         System.out.println(first_name.concat("Kaveri"));
+
+            //Concept of stack and heap in strings
+            String lastName = "navin" ;  //one object value in heap(String constant pool) with a address
+            lastName = lastName+" reddy"; // lastname doesn't get changed a new object is created in pool with different address for  navin reddy
+            System.out.println(lastName);
+            //String new_lastName = lastName+" reddy";
+            //System.out.println(lastName==new_lastName);  // will come false
+
+            String s1 = "Navin";
+            String s2 = "Navin";
+            //both will have same addresses as only one object is created for Navin(as when s1 was created Navin got stored in pool and when s2 was created as Navin was already present in pool no new object with different address was created)
+
+            System.out.println(s1==s2);  // will come true
+
+            //the lastname= navin object will be then eligible for garbage collection
     }
+
+
 }
+
+
