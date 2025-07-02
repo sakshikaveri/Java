e73c5e10ab00f235ec0bb95b8c9b6dfb414590f6
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2025-07-02 20:33:53 +0530

    Array of objects

---
+++ ArrayOfObjects.java
@@ -0,0 +1,29 @@
+
+class Student{
+
+    //instance variables 
+    int age;
+    String name;
+}
+
+public class ArrayOfObjects{
+
+    public static void main(String[] args) {
+        
+        Student s1 = new Student();
+        s1.age = 22;
+        s1.name = "Sakshi";
+
+        Student s2 = new Student();
+        s2.age = 54;
+        s2.name="Monica";
+
+        Student students[] = new Student[2];  //here you're not creating array of objects of class Student you've to define objects manually and then store them in array of type "yourclass".
+        students[0] = s1;
+        students[1] = s2;
+
+        for(int i=0;i<students.length;i++){
+            System.out.println("Name:"+students[i].name+" Age:"+students[i].age);
+        }
+    }
+}
