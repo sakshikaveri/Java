b01991cc66373b2aa21addc9a600fdf0f2b7ab99
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2025-07-03 16:06:25 +0530

    instantiating static variables

--- StaticVariable.java
+++ StaticVariable.java
@@ -12,8 +12,26 @@ class Mobile{
     }
 }
 
+class Car{
+    int price;
+    String brand;
+    static String vehicle;
+
+    public Car(){
+        price = 150000;
+        brand = "";
+        System.out.println("Inside CAR constructor");
+        //vehicle="car";  // no need to write it in constructor as it's a static variables whenever obj is instantiated constructor is called and variables are instantiated, don't need that for tatic variable
+    }
+
+    static{
+        vehicle ="car";
+        System.out.println("inside static block");  // only gets instantiated when class loads
+    }
+}
+
 public class StaticVariable {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         
         Mobile obj1 = new Mobile();
         obj1.brand="Apple";
@@ -29,5 +47,15 @@ public class StaticVariable {
         Mobile.name = "Android";  //both obj1 and obj2 will have name as Android
         obj1.show();
         obj2.show();
+
+        Car objCar = new Car();  // whenever obj is created constructor is called and instance variables are initiated
+        System.out.println(objCar.price);
+
+        Car newobjCar = new Car();
+
+        //if no objects are created static and constructor both are not called hence,
+        Class.forName("Car");  // used for loading class
+
+
     }
 }
