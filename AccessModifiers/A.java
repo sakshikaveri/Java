4bce19dafa1f6f8ecad684c4e164bade89730f16
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2025-07-24 18:55:52 +0530

    private access modifier

--- AccessModifiers/A.java
+++ AccessModifiers/A.java
@@ -1,7 +1,21 @@
 package AccessModifiers;
 
 public class A {
-    
-    //int number;  //cannot be accessed in other package
-    public int number;  //now it can be accessed in other package 
+
+    // int number; //cannot be accessed in other package
+    // public int number; //now it can be accessed in other package
+
+    private int number; // can be used in it's own class
+
+
+    public static void main(String[] args) {
+
+            class B extends A{
+            B obj = new B();
+           // System.out.println(obj.number);  // will give error as number can be used only in class A and not in class B
+    }
+            A obj = new A();
+            System.out.println(obj.number);
+}
+
 }
