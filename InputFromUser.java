59ae33469f96bfff0247078b9bd5abd5994cadb0
Sakshi Kaveri <sakshikaveri2002@gmail.com>
2025-08-10 23:20:18 +0530

    reading user input using scanner class

--- InputFromUser.java
+++ InputFromUser.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class InputFromUser {
     public static void main(String[] args) throws NumberFormatException, IOException {
 
         System.out.print("Enter a number: ");
 
-        // old method of reading a user input number
+        /* old method of reading a user input number
 
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
@@ -15,6 +16,12 @@ public class InputFromUser {
         int num = Integer.parseInt(br.readLine());
         System.out.println(num);
 
-        br.close();
+        br.close();*/
+
+        // Current method
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(number);
     }
 }
